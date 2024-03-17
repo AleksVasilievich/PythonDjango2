acd2b1e250bc31c24a236ed26ac3ff95851abc3d
AleksVasilievich <104069090+AleksVasilievich@users.noreply.com>
2024-03-17 09:38:17 +0300

    preparation for deploymentÐ

--- recipes_app/forms.py
+++ recipes_app/forms.py
@@ -6,13 +6,20 @@ from django.contrib.auth.models import User
 
 
 class RecipeForm(forms.ModelForm):
+    title = forms.CharField(label='Заголовок', widget=forms.TextInput(attrs={'class': 'large-text-input'}))
+    description = forms.CharField(label='Описание', widget=forms.Textarea(attrs={'class': 'large-textarea'}))
+    preparation_steps = forms.CharField(label='Шаги приготовления',
+                                        widget=forms.Textarea(attrs={'class': 'large-textarea'}))
+    preparation_time = forms.IntegerField(label='Время приготовления')
+    image = forms.ImageField(label='Изображение')
+    categories = forms.CharField(label='Категории')
+
     class Meta:
         model = Recipe
         fields = ['title', 'description', 'preparation_steps', 'preparation_time', 'image', 'categories']
         widgets = {
             'image': forms.FileInput(attrs={'accept': 'image/*'})
         }
-
 
 
 class SignUpForm(UserCreationForm):
